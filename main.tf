7c02a9e46e99dd2c02e4e191065ae1d366c3f851
jasongomes <jason140294@gmail.com>
2025-01-23 23:44:24 +0530

    Adding files to Github repo

---
+++ main.tf
@@ -0,0 +1,92 @@
+resource "aws_s3_bucket" "website_bucket" {
+  bucket = var.bucket_name
+}
+
+resource "aws_s3_object" "index_html" {
+  bucket = aws_s3_bucket.website_bucket.id
+  key = "index.html"
+  source = "website/index.html"
+  etag = filemd5("website/index.html")
+  content_type = "text/html"
+}
+
+resource "aws_s3_object" "error_html" {
+  bucket = aws_s3_bucket.website_bucket.id
+  key = "error.html"
+  source = "website/error.html"
+  etag = filemd5("website/error.html")
+  content_type = "text/html"
+}
+
+resource "aws_cloudfront_origin_access_identity" "origin_access_identity" {
+  comment = "Origin Access Identity for static website"
+  
+}
+
+resource "aws_cloudfront_distribution" "cloudfront_distribution" {
+  origin {
+    domain_name = aws_s3_bucket.website_bucket.bucket_regional_domain_name
+    origin_id = var.bucket_name
+
+    s3_origin_config {
+       origin_access_identity = aws_cloudfront_origin_access_identity.origin_access_identity.cloudfront_access_identity_path
+    }
+  }
+
+  enabled = true
+  is_ipv6_enabled = true
+  default_root_object = var.website_index_document
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = var.bucket_name
+
+    forwarded_values {
+      query_string = false
+
+      cookies {
+        forward = "none"
+      }
+    }
+
+
+    viewer_protocol_policy = "allow-all"
+    min_ttl                = 0
+    default_ttl            = 3600
+    max_ttl                = 86400
+  }
+
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  
+  }
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+  tags = {
+    name = "Cloudfront Distribution"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_policy" "website_bucket_policy" {
+  bucket = aws_s3_bucket.website_bucket.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action   = "s3:GetObject"
+        Effect   = "Allow"
+        Resource = "${aws_s3_bucket.website_bucket.arn}/*"
+        Principal = {
+          CanonicalUser = aws_cloudfront_origin_access_identity.origin_access_identity.s3_canonical_user_id
+        }
+      }
+    ]
+  })
+    
+}
---
+++ variables.tf
@@ -0,0 +1,10 @@
+variable "bucket_name" {
+  #type = string
+  description = "This is the bucket name for S3"
+  default = "myjason2345gomes"
+}
+
+variable "website_index_document" {
+  description = "This is the Website index document"
+  default = "index.html"
+}
